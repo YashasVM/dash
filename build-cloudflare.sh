335a0620c1d70e8ab7c0a13753091b149f6bcd20
YashasVM <admin5@duck.com>
2026-09-10 22:17:51 +0530

    Fix production 404 game assets and controls

--- build-cloudflare.sh
+++ build-cloudflare.sh
@@ -6,9 +6,10 @@ npm --prefix docs-site run build
 
 rm -rf .pages-dist
 mkdir -p .pages-dist
-cp index.html styles.css script.js favicon.svg 404.html linkedin-preview.png preview.png .pages-dist/
+cp index.html styles.css script.js 404.js favicon.svg 404.html linkedin-preview.png preview.png .pages-dist/
 cp -R fonts .pages-dist/fonts
 cp -R logos .pages-dist/logos
+cp -R assets .pages-dist/assets
 cp -R functions .pages-dist/functions
 
 for path in _next api docs llms.mdx og; do
